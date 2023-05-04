406a025a23809a1cf7c719dcf425cf86f55aafa1
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-04 23:53:14 +0300

    Initial commit

--- HW3.6.1.cpp
+++ HW3.6.1.cpp
@@ -1,20 +1,91 @@
-﻿// HW3.6.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include <cstring>
+#include "Math_function.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    double firstNumber = 0;
+    double secondNumber = 0;
+    int operationSelection = 0;
+    std::string nameOfOperation;
+    bool checkingValue = false;
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+    std::cout << "Введите первое число: ";
+    std::cin >> firstNumber;
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    std::cout << "Введите второе число: ";
+    std::cin >> secondNumber;
+
+    do
+    {
+        std::cout << "Выберите операцию (1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
+        std::cin >> operationSelection;
+
+        if (operationSelection == 4 && secondNumber == 0)
+        {
+            std::cout << " Делить на ноль нельзя... " << std::endl;
+        }
+        else if (operationSelection <= 5 && operationSelection >= 1)
+        {
+            checkingValue = true;
+        }
+        else
+        {
+            std::cout << " Плохой выбор. Выберете еще разок... " << std::endl;
+        }
+    } while (!checkingValue);
+
+    switch (operationSelection)
+    {
+    case(1):
+    {
+        nameOfOperation = "плюс";
+        break;
+    }
+    case(2):
+    {
+        nameOfOperation = "минус";
+        break;
+    }
+    case(3):
+    {
+        nameOfOperation = "умножить";
+        break;
+    }
+    case(4):
+    {
+        nameOfOperation = "разделить";
+        break;
+    }
+    case(5):
+    {
+        nameOfOperation = "в степени";
+        break;
+    }
+    };
+
+    std::cout << firstNumber << " " << nameOfOperation << " " << secondNumber << " = ";
+
+    if (operationSelection == 1)
+    {
+        std::cout << addition(firstNumber, secondNumber);
+    }
+    else if (operationSelection == 2)
+    {
+        std::cout << subtraction(firstNumber, secondNumber);
+    }
+    else if (operationSelection == 3)
+    {
+        std::cout << multiplication(firstNumber, secondNumber);
+    }
+    else if (operationSelection == 4)
+    {
+        std::cout << division(firstNumber, secondNumber);
+    }
+    else
+    {
+        std::cout << exponentiation(firstNumber, secondNumber);
+    };
+};
--- Math_function.h
+++ Math_function.h
@@ -1,1 +1,6 @@
 #pragma once
+double addition(double firstNumber, double secondNumber);
+double subtraction(double firstNumber, double secondNumber);
+double multiplication(double firstNumber, double secondNumber);
+double division(double firstNumber, double secondNumber);
+double exponentiation(double firstNumber, double secondNumber);
